0483df0ee1f87928726eb0394f4c477f5206d881
YusufAfacan <y.afacan@outlook.com.tr>
2024-01-02 20:22:27 +0300

    -Adaptive Camera
    -Adaptive Camera
    Camera projects same area in every mobile phone resolution

---
+++ Assets/Scripts/CameraViewportHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewportHandler : MonoBehaviour
+{
+    public SpriteRenderer cameraSightArea;
+
+    // Use this for initialization
+    void Start()
+    {
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = cameraSightArea.bounds.size.x / cameraSightArea.bounds.size.y;
+
+        if (screenRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = cameraSightArea.bounds.size.y / 2;
+        }
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = cameraSightArea.bounds.size.y / 2 * differenceInSize;
+        }
+    }
+}
